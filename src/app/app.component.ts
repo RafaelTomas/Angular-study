3894ca7d96a78808260add8561174857ed1babd1
Rafael Tomas <rafaeltomassouza@gmail.com>
2023-04-22 13:05:34 -0300

    create table dynamic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,108 +1,83 @@
-import {Component, OnInit} from '@angular/core';
- interface Exam {
-   field: string;
-   value: any;
- }
-interface  ExamList {
-  date: string;
-  exams: Exam[];
+import { Component, ViewChild, ElementRef, OnInit  } from '@angular/core';
+import { FormGroup, FormControl,FormArray, FormBuilder } from '@angular/forms'
+
+interface examsPlus {
+  exam: string;
+  result: string;
 }
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-   examList: ExamList[] = [
-     {
-       date: '2023-03-10',
-       exams: [
-         {
-           field: "Hemoglobina Glicada",
-           value: "5,5%"
-         },
-         {
-           field: "Colesterol Total",
-           value: "180 mg/dL"
-         },
-         {
-           field: "Creatinina",
-           value: "0,9 mg/dL"
-         },
-         {
-           field: "Plaquetas",
-           value: "250.000 /mm³"
-         },
-         {
-           field: "Teste",
-           value: "0,9 mg/dL"
-         }
-       ]
-     },
-     {
-       date: '2022-12-01',
-       exams: [
-         {
-           field: "Glicemia em Jejum",
-           value: "95 mg/dL"
-         },
-         {
-           field: "Triglicerídeos",
-           value: "100 mg/dL"
-         },
-         {
-           field: "HDL",
-           value: "55 mg/dL"
-         },
-         {
-           field: "LDL",
-           value: "120 mg/dL"
-         }
-       ]
-     },
-     {
-       date: '2022-08-15',
-       exams: [
-         {
-           field: "Hemácias",
-           value: "4.000.000 /mm³"
-         },
-         {
-           field: "Leucócitos",
-           value: "10.000 /mm³"
-         },
-         {
-           field: "Plaquetas",
-           value: "250.000 /mm³"
-         }
-       ]
-     }
-   ];
-    examFields = [""];
-  constructor(){}
+export class AppComponent implements OnInit {
 
-  ngOnInit(): void {
-    this.examFields = this.examList.reduce((acc: string[], curr) => {
-      curr.exams.forEach(e => {
-        if (!acc.includes(e.field)) {
-          acc.push(e.field);
-        }
-      });
-      return acc;
-    }, []);
-
-
-  }
-
-  getValueByFieldAndDate(field: string, date: string): string {
-    const examData = this.examList.find(e => e.date === date);
-    if (examData) {
-      const exam = examData.exams.find(e => e.field === field);
-      if (exam) {
-        return exam.value;
+  title = 'FormArray Example in Angular Reactive forms';
+    GetExamsPlusPreConfigured: examsPlus[]  = [
+      {
+        exam: 'Força',
+        result: ''
+      },
+      {
+        exam: 'Foco',
+        result: ''
+      },
+      {
+        exam: 'Fé',
+        result: ''
       }
-    }
-    return '-';
+  ]
+  examsName: any[];
+// @ts-ignore
+  examForm: FormGroup;
+
+
+  constructor(private fb:FormBuilder) {}
+
+  ngOnInit(){
+    this.examForm = this.fb.group({
+      date: '',
+      examsPlus: this.fb.array([]) ,
+      examsConfig: this.fb.array([]) ,
+    });
+    this.GetExamsPlusPreConfigured.forEach(res => {
+      this.examsConfig().push(this.fb.group({
+        exam: [res.exam],
+        result: ['']
+      }));
+    });
   }
+
+  examsPlus() : FormArray {
+    return this.examForm.get("examsPlus") as FormArray
+  }
+
+  examsConfig(): FormArray {
+    return this.examForm.get('examsConfig') as FormArray
+  }
+
+  newExamPlus(): FormGroup {
+   return this.fb.group({
+      exam: '' ,
+      result: '',
+   })
+  }
+
+  addExams() {
+    this.examsPlus().push(this.newExamPlus());
+  }
+
+  removeExamPlus(i:number) {
+    this.examsPlus().removeAt(i);
+  }
+
+  onSubmit() {
+    console.log(this.examForm.value);
+    console.log(this.examsName)
+  }
+
 
 }
+
+
